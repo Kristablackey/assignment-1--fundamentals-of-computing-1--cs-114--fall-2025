2d2d16fb72c679a99fd3ad724239db11ba71695d
Kristablackey <blackey@hartford.edu>
2025-09-18 13:16:35 -0400

    Assignment 1

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -1,5 +1,31 @@
-public class Assignment1Program1 {
-  public static void main(String[] args) {
+import java.util.Random;
 
-  }
-}
+public static void printInitials () {
+
+print("K    K   BBBBB");
+print("K   K    B    B");
+print("K  K     B   B");
+print("KKK      BBBBB");
+print("K  K     B   B");
+print("K   K    B    B");
+print("K    K   BBBBB");
+
+// Step 1: 5-Character String
+string s = input("krsle");
+string a = s.substring(1,s.length()-1);
+char b = a.charAt(0);
+char c = a.charAt(1);
+char d = a.charAt(2);
+string e = d + c + b;
+
+// Step 2: Fahrenheit and convert to Celsius
+float f = (input("87"));
+cel = (f - 32) * 5/9;
+
+// Step 3: Random number Generator
+r = random.randint(32, 16384);
+
+// Step 4: Combine
+system.out.println(cel + e + r ); }
+
+30.555555555555557lsr15327
--- program-2/Assignment1Program2.java
+++ program-2/Assignment1Program2.java
@@ -1,5 +1,22 @@
-public class Assignment1Program2 {
-  public static void main(String[] args) {
+// Step 1: Base Input
+int base = 4;
+print(5, base);
 
-  }
-}
+// Step 2: Max Value
+int maxValue = Math.pow(base, 4) - 1;
+print("1232132123, base, maxValue,");
+
+// Step 3: Number Input
+int num=(input("123"));
+
+// Step 4: Conversion
+int d1 = num; // Math.pow(base, 3)
+int num = num % Math.pow(base,3);
+
+int d2 = num; // Math.pow(base,2)
+int num = num % Math.pow(base,2);
+
+int d3 = num; // base
+int d4 = num % base;
+
+print(f"{num} (base 10) = {d1}{d2}{d3}{d4} (base {base})");
